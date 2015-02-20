266c7c593e2c90af6558ff7a0dea4157d94f68a1
namlook <nicolas.clairon@elkorado.com>
2015-02-20 16:14:44 +0100

    keep up with new eureka

--- addon/index.js
+++ addon/index.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 import WidgetApplication from 'ember-eureka/widget-application';
 
 export default WidgetApplication.extend({
-    name: Ember.computed.alias('application.name')
+    name: Ember.computed.alias('applicationController.name')
 });
